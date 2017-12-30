8ca5a51b7ae8c53bc4f262dd79360a0d79059452
smartmix <tiziano@smartmix.it>
2017-12-30 09:58:18 +0100

    Startup script

---
+++ Labelled Pages exporter.js
@@ -0,0 +1,168 @@
+﻿/**
+ *
+ * @name Labelled Pages exporter
+ * @desc Recognise page color labels and choose which export to pdf
+ * @version 0.0.4
+ *
+ * @author Smart Mix smartmix.it
+ * @link https://smartmix.it
+ * 
+ * 
+ * @see https://forums.adobe.com/thread/2173129
+ 
+ 
+ */
+
+
+
+
+var nome = "Labelled Pages exporter";
+var version = '0.0.4';
+
+
+
+
+//import script language preferences 
+var scriptPath = getScriptPath().parent.fsName;
+$.evalFile(scriptPath+'/lang.js');
+
+var labelsName = textContent['labelsName'];
+var ui = textContent['ui'][lang];
+
+app.scriptPreferences.userInteractionLevel = UserInteractionLevels.interactWithAll;
+
+main();
+
+function main(){
+    finestra ();
+}
+
+
+//user inteface
+function finestra(){
+	 
+	if(!app.documents.length){
+		alert (ui['noDocumentOpen']);  
+		exit(); 
+	}else{
+		
+		var pagesSelection = getPageSelectionObj();
+		var myReturn = '';
+		
+		var w = new Window ("dialog", nome+' '+version);
+		
+		var selector = w.add('panel',undefined,ui['selectorPanel']);
+		selector.orientation = 'row';		
+		
+		
+		var labelList = selector.add ("listbox", undefined, pagesSelection.labels, {multiselect: true});
+		labelList.preferredSize = [200,200];
+		
+		var buttongroup = w.add('group');
+
+		var exp = buttongroup.add ("button", undefined, ui['exportButton']);
+		var chiudi = buttongroup.add ("button", undefined, ui['closeButton']);
+		
+		chiudi.onClick = function(){w.close();}
+		
+		exp.onClick = function(){
+			w.close();
+			myReturn = true;
+		};
+		
+		w.show ();
+		
+		if (myReturn == true){
+			
+			var pagine = [];
+			
+			for(b=0; b< labelList.selection.length; b++){
+				pagine.push(pagesSelection[labelList.selection[b]]);
+			}
+			
+			exportPDF(pagine);
+			
+		}
+	}	
+}
+
+
+
+
+
+
+
+function getPageSelectionObj(){
+	var curDoc = app.documents[0];  
+	var allPages = curDoc.pages;
+	
+	var pagesSelection = {};
+    pagesSelection.labels = [];
+	//pagesSelection.pages = {};
+	var checkLabelExist = false;
+	
+	
+	for(i=0; i<allPages.length; i++){
+		var curPage = allPages[i];
+		var pColor = curPage.pageColor.toString();
+		
+		if(labelsName[lang][pColor]!=undefined){
+			var coolColorName = labelsName[lang][pColor];
+		}else{
+			var coolColorName = pColor;
+		}
+		
+		var pageNumb = curPage.documentOffset + 1;
+		
+		
+		for (c = 0; c< pagesSelection.labels.length; c++){
+			if(pagesSelection.labels[c]==coolColorName){
+				checkLabelExist = true;
+			}
+		}
+		
+		if(checkLabelExist==false){
+			pagesSelection.labels.push(coolColorName);
+			pagesSelection[coolColorName]=["+" + pageNumb];
+		}else{
+			pagesSelection[coolColorName].push("+" + pageNumb);
+		}
+		checkLabelExist = false;
+	}	
+	return pagesSelection;	
+}
+
+
+
+function exportPDF(pagine){
+	
+	var theFolder = Folder.selectDialog(ui['ChooseFolder']);  
+	if (theFolder == null) {
+		exit();  
+	}
+		
+	app.pdfExportPreferences.pageRange = pagine.join(",");
+	var curDoc = app.documents[0]; 
+	var pdfName = curDoc.name.replace(/.indd$/,".pdf");
+	var theFile = File(theFolder + "/" + pdfName);
+	
+	
+	try {  
+		curDoc.exportFile(ExportFormat.PDF_TYPE , theFile , true);
+		
+	}catch(e) {  
+		alert(e);  
+	}
+	
+	app.pdfExportPreferences.pageRange = "";
+	
+}
+
+
+function getScriptPath() {
+	try { 
+    return app.activeScript; 
+  } catch(e) { 
+    return File(e.fileName); 
+  }
+}
---
+++ lang.js
@@ -0,0 +1,61 @@
+var lang = 'it'; //for english version change this in var lang = 'en';
+
+var textContent = {
+	'labelsName':{
+		'en':{
+			'USE_MASTER_COLOR':'Use master color',
+			'WHITE':'White',
+			'GOLD':'Gold',
+			'PINK':'Pink',
+			'YELLOW':'Yellow',
+			'GREEN':'Green',
+			'LIGHT_BLUE':'Light blue',
+			'LAVENDER':'Lavender',
+			'GRAY':'Gray',
+			'RED':'Red',
+			'ORANGE':'Orange',
+			'TAN':'Tan',
+			'DARK_GREEN':'Dark green',
+			'BLUE':'Blue',
+			'VIOLET':'Violet',
+			'BLACK':'Black',
+			'NOTHING':'Nothing',
+			'ChooseFolder':'Choose a folder for export'
+		},
+		'it':{
+			'USE_MASTER_COLOR':'Usa colore mastro',
+			'WHITE':'Bianco',
+			'GOLD':'Oro',
+			'PINK':'Rosa',
+			'YELLOW':'Giallo',
+			'GREEN':'Verde',
+			'LIGHT_BLUE':'Blu chiaro',
+			'LAVENDER':'Lavanda',
+			'GRAY':'Grigio',
+			'RED':'Rosso',
+			'ORANGE':'Arancione',
+			'TAN':'Cannella',
+			'DARK_GREEN':'Verde scuro',
+			'BLUE':'Blu',
+			'VIOLET':'Viola',
+			'BLACK':'Nero',
+			'NOTHING':'Nessuna',
+			'ChooseFolder':'Scegli la cartella di esportazione'
+		}
+	},
+	
+	'ui':{
+		'it':{
+			'selectorPanel':'Scegli l\'etichetta da esportare',
+			'exportButton':'Esporta PDF',
+			'closeButton':'Chiudi',
+			'noDocumentOpen':'Nessun documento aperto'
+		},
+		'en':{
+			'selectorPanel':'Choose the label to export',
+			'exportButton':'Export PDF',
+			'closeButton':'Close',
+			'noDocumentOpen':'No document open'
+		}		
+	}	
+}
